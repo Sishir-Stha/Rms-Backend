46ae0ecc9ba8fde64ce90a3f8343c0d1ce1012f0
Sishir Shrestha <sishershrestha000@gmail.com>
2026-05-07 09:09:57 +0545

    to deploy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import { pool } from "../src/utills/pool"
+import { pool } from "./utills/pool"
 import { router } from "./root.routes"
 import cors from "cors";
 
